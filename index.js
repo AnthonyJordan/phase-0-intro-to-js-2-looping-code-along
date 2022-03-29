b3e6e65902240eb8342bb42d3a524f255b667a3e
Anthony <Kain235@gmail.com>
2022-03-29 14:52:58 -0400

    commit tree 3b6955d9f5ee236fe1cdfe31955b8cf4ded36b9b

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(people, message){
+    const messageArray = [];
+    for(let i = 0; i < people.length; i++){
+        messageArray.push(`Thank you, ${people[i]}, for the wonderful ${message} gift!`)
+    }
+    return messageArray;
+}
+
+function countDown(n) {
+    while(n > -1){
+        console.log(n)
+        n--;
+    }
+}
